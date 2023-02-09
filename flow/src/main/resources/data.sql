0887c30eafe9087ed771e85c34b71afdbeb314cf
Kwonnamhyung <rnjsskagud@gmail.com>
2023-02-09 19:16:55 +0900

    고정 확장자 초기 데이터 설계

---
+++ flow/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into extension(id , name, is_checked) values (1 , 'bat' , false);
+insert into extension(id , name, is_checked) values (2 , 'cmd' , false);
+insert into extension(id , name, is_checked) values (3 , 'com' , false);
+insert into extension(id , name, is_checked) values (4 , 'cpl' , false);
+insert into extension(id , name, is_checked) values (5 , 'exe' , false);
+insert into extension(id , name, is_checked) values (6 , 'scr' , false);
+insert into extension(id , name, is_checked) values (7 , 'js' , false);
